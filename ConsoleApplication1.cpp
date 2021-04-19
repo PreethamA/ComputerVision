923ddbf29578771e02429ce5e2843cea1fb9332c
Preetham.Armoor <45017006+PreethamA@users.noreply.github.com>
2021-04-19 17:14:01 +0200

    VideoCapture

--- ConsoleApplication1.cpp
+++ ConsoleApplication1.cpp
@@ -1,44 +1,56 @@
-#include "opencv2/imgproc.hpp"
-#include "opencv2/imgcodecs.hpp"
-#include "opencv2/highgui.hpp"
+#include <opencv2/core.hpp>
+#include <opencv2/videoio.hpp>
+#include <opencv2/highgui.hpp>
 #include <iostream>
+#include <stdio.h>
 using namespace cv;
-Mat src, dst;
-int morph_elem = 0;
-int morph_size = 0;
-int morph_operator = 0;
-int const max_operator = 4;
-int const max_elem = 2;
-int const max_kernel_size = 21;
-const char* window_name = "Morphology Transformations Demo";
-void Morphology_Operations(int, void*);
-int main(int argc, char** argv)
+using namespace std;
+int main(int, char**)
 {
-    CommandLineParser parser(argc, argv, "{@input | baboon.jpg | input image}");
-    src = imread(samples::findFile(parser.get<String>("@input")), IMREAD_COLOR);
-    if (src.empty())
-    {
-        std::cout << "Could not open or find the image!\n" << std::endl;
-        std::cout << "Usage: " << argv[0] << " <Input image>" << std::endl;
-        return EXIT_FAILURE;
+    Mat src;
+    // use default camera as video source
+    VideoCapture cap(0);
+    // check if we succeeded
+    if (!cap.isOpened()) {
+        cerr << "ERROR! Unable to open camera\n";
+        return -1;
     }
-    namedWindow(window_name, WINDOW_AUTOSIZE); // Create window
-    createTrackbar("Operator:\n 0: Opening - 1: Closing  \n 2: Gradient - 3: Top Hat \n 4: Black Hat", window_name, &morph_operator, max_operator, Morphology_Operations);
-    createTrackbar("Element:\n 0: Rect - 1: Cross - 2: Ellipse", window_name,
-        &morph_elem, max_elem,
-        Morphology_Operations);
-    createTrackbar("Kernel size:\n 2n +1", window_name,
-        &morph_size, max_kernel_size,
-        Morphology_Operations);
-    Morphology_Operations(0, 0);
-    waitKey(0);
+    // get one frame from camera to know frame size and type
+    cap >> src;
+    // check if we succeeded
+    if (src.empty()) {
+        cerr << "ERROR! blank frame grabbed\n";
+        return -1;
+    }
+    bool isColor = (src.type() == CV_8UC3);
+    //--- INITIALIZE VIDEOWRITER
+    VideoWriter writer;
+    int codec = VideoWriter::fourcc('M', 'J', 'P', 'G');  // select desired codec (must be available at runtime)
+    double fps = 25.0;                          // framerate of the created video stream
+    string filename = "./live.avi";             // name of the output video file
+    writer.open(filename, codec, fps, src.size(), isColor);
+    // check if we succeeded
+    if (!writer.isOpened()) {
+        cerr << "Could not open the output video file for write\n";
+        return -1;
+    }
+    //--- GRAB AND WRITE LOOP
+    cout << "Writing videofile: " << filename << endl
+        << "Press any key to terminate" << endl;
+    for (;;)
+    {
+        // check if we succeeded
+        if (!cap.read(src)) {
+            cerr << "ERROR! blank frame grabbed\n";
+            break;
+        }
+        // encode the frame into the videofile stream
+        writer.write(src);
+        // show live and wait for a key with timeout long enough to show images
+        imshow("Live", src);
+        if (waitKey(5) >= 0)
+            break;
+    }
+    // the videofile will be closed and released automatically in VideoWriter destructor
     return 0;
-}
-static void Morphology_Operations(int, void*)
-{
-    // Since MORPH_X : 2,3,4,5 and 6
-    int operation = morph_operator + 2;
-    Mat element = getStructuringElement(morph_elem, Size(2 * morph_size + 1, 2 * morph_size + 1), Point(morph_size, morph_size));
-    morphologyEx(src, dst, operation, element);
-    imshow(window_name, dst);
 }
